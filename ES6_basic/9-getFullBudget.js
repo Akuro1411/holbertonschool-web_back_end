9ab0e187e5b6b44acc4b1aa5fee95105b1c5346b
Akuro1411 <nahidisayev147@gmail.com>
2024-07-21 09:09:58 +0000

    Budget.income must be

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -3,10 +3,10 @@ import getBudgetObject from './7-getBudgetObject';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   budget.getIncomeInDollars = function (income) {
-    return `$${income}`;
+    return `$${budget.income}`;
   };
   budget.getIncomeInEuros = function (income) {
-    return `${income} euros`;
+    return `${budget.income} euros`;
   };
   return budget;
 }
