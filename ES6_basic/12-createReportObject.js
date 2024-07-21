4af45ce19cc9f6173de9de1acf7e1165ca3898f3
Akuro1411 <nahidisayev147@gmail.com>
2024-07-21 19:20:35 +0000

    Report object

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,14 +1,9 @@
+/* eslint-disable */
 export default function createReportObject(employeesList) {
-  const object = employeeslist;
-  object.allEmployees = function(){
-    return object;
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
   };
-  object.getNumberOfDepartments = function(obj){
-    let total = 0;
-    for (const i of obj){
-      total += 1;
-    };
-    return total
-  };
-  return object;
 }
