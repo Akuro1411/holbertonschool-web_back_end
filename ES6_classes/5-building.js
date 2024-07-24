948736e44595bc9c937e7923358ee0f3ab9e4181
Akuro1411 <nahidisayev147@gmail.com>
2024-07-24 20:50:19 +0000

    Function must not created for correct output

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    this.sqft = sqft;
+    if (this.constructor !== Building && typeof (this.evacuationWarningMessage) !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newNumber) {
+    this._sqft = newNumber;
+  }
+  // evacuationWarningMessage() {} //If you write this function again the error won't be appear.}
+}
