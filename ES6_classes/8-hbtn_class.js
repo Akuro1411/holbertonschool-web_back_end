9d2bf8a3acf3c11afdd9421577993b93827aed17
Akuro1411 <nahidisayev147@gmail.com>
2024-07-27 12:40:05 +0000

    Use symbol for method

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,15 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._location = location;
+    this._size = size;
+  }
+  [Symbol.toPrimitive](func) {
+    if (func === 'string') {
+      return this._location;
+    }
+    if (func === 'number') {
+      return this._size;
+    }
+    return 1
+  }
+}
