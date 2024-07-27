a411e09daa6bb04a487bd374d3c3928691d64f6b
Akuro1411 <nahidisayev147@gmail.com>
2024-07-27 17:29:37 +0000

    Symbol species, help me!!

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    // const Object = this.constructor[Symbol.species];
+    // return new Object(this._brand, this._motor, this._color);
+    return new this.constructor[Symbol.species](this._brand, this._motor, this._color) //You can write return with this type, too.
+  }
+}
