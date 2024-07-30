a3fa1f4f55559f675193f83391226413e76e9cf0
Akuo1411 <nahidisayev147@gmail.com>
2024-07-30 13:57:38 +0000

    Data is returned with dataview

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  const arr_buff = new ArrayBuffer(length);
-  const int8view = new Int8Array(arr_buff);
+  const arrBuff = new ArrayBuffer(length);
+  const int8view = new Int8Array(arrBuff);
   if (position > length) {
     throw Error('Position outside range');
-  }
-  else {
+  } else {
     int8view[position] = value;
   }
-  return arr_buff;
+  return new DataView(arrBuff);
 }
