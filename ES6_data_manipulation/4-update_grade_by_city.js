1a3f9eeabd1f352577b908b9624575d970959673
Akuo1411 <nahidisayev147@gmail.com>
2024-07-30 15:02:58 +0000

    New versioneslint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,23 +1,20 @@
 export default function updateStudentGradeByCity(obj, city, newGrades) {
-  if (!Array.isArray(obj) || !Array.isArray(newGrades))
-  {
+  if (!Array.isArray(obj) || !Array.isArray(newGrades)) {
     return [];
   }
-  const arr = obj.filter((row) => row.location === city)
-  const secondArr = arr.map((row) => 
+  const arr = obj.filter((row) => row.location === city);
+  const secondArr = arr.map((row) =>
   {
     for (const line of newGrades){
       if (row.id === line.studentId) {
         row.grade = line.grade;
         break;
-      }
-      else {
-        row.grade = 'N/A'
+      } else {
+        row.grade = 'N/A';
       }
     }
-    return row
+    return row;
   }
 );
-  
   return secondArr;
 }
