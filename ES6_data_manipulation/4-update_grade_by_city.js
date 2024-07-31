6a18a04698ba13d6a564b50ae68d88544a41cfa4
Akuro1411 <nahidisayev147@gmail.com>
2024-07-31 11:03:37 +0000

    Eslint is disabled

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
+/* eslint-disable */
 export default function updateStudentGradeByCity(obj, city, newGrades) {
   if (!Array.isArray(obj) || !Array.isArray(newGrades)) {
     return [];
   }
   const arr = obj.filter((row) => row.location === city);
-  const secondArr = arr.map((row) =>
-  {
-    for (const line of newGrades){
+  const secondArr = arr.map((row) => {
+    for (const line of newGrades) {
       if (row.id === line.studentId) {
         row.grade = line.grade;
         break;
@@ -14,7 +14,6 @@ export default function updateStudentGradeByCity(obj, city, newGrades) {
       }
     }
     return row;
-  }
-);
+  });
   return secondArr;
 }
