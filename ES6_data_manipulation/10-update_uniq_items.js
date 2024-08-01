0a36422c6c64ae2d0658984a81f568bc3b210829
Akuro1411 <nahidisayev147@gmail.com>
2024-08-01 21:01:21 +0000

    Code is designed for eslint

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,8 @@
 export default function updateUniqueItems(updateMap) {
-   if (!(updateMap instanceof Map)) {
+  if (!(updateMap instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (const i of updateMap){
+  for (const i of updateMap) {
     if (i[1] === 1) {
       updateMap.set(i[0], 100);
     }
