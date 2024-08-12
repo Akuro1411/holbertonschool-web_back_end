2946a66855609345ad9107480ac859b4209a2726
Akuro1411 <nahidisayev147@gmail.com>
2024-08-12 17:42:08 +0000

    Files were pushed to wrong repo

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+def concat(str1: str, str2: str) -> str:
+    new_str = str1 + str2
+    return new_str
---
+++ python_variable_annotations/README.md
@@ -0,0 +1,3 @@
+Hello!
+Description: This folder contains files about annotations in python <br>
+Author: Nahid Isayev
