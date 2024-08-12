dc51b6ade62574cf49efeaa337e4b7d02d41a007
Akuro1411 <nahidisayev147@gmail.com>
2024-08-12 18:19:51 +0000

    Sum of floats and integers

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Typing module is imported
+"""
+import typing
+
+
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[float, int]]) -> float:
+    """
+    Function returns sum of floats and integers list
+    """
+    new_sum = sum(mxd_lst)
+    return new_sum
