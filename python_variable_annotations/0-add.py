0309d8ac132b5096cbe9f6b8229ce31c1496d6fb
Akuro1411 <nahidisayev147@gmail.com>
2024-08-12 17:45:55 +0000

    Code is designed for pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+"""
+No module
+"""
 def add(a: float, b: float) -> float:
+    """
+    The function adds to float and finds their sum
+    """
     num = a + b
     return num
 
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+No module
+"""
 def concat(str1: str, str2: str) -> str:
+    """
+    The function concats two string
+    """
     new_str = str1 + str2
     return new_str
