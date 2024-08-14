99de06acc7482b9ddbcb87f34a062a71bbbcdb51
Akuro1411 <nahidisayev147@gmail.com>
2024-08-14 12:55:28 +0000

    Wait until random time

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Random and asyncio modules are imported
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """
+    Function waits until random float time between 0 and max_delay
+    """
+    wait_time = random.uniform(0, max_delay)
+    await asyncio.sleep(wait_time)
+
+    return wait_time
---
+++ python_async_function/README.md
@@ -0,0 +1,3 @@
+Hello dear reader! <br>
+Description: This directory contains files with async functions in python <br>
+Author: Nahid Isayev
