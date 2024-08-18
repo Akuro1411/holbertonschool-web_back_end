78333ea9a2558e1157ce28e09364de64f00e186a
Akuro1411 <nahidisayev147@gmail.com>
2024-08-18 12:39:59 +0000

    Function will be called

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Random and asyncio modules are imported
+"""
+import asyncio
+import random
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    :param n:
+    :param max_delay:
+    :return: Returns the list of delay time with asceding order
+    """
+    new_list = []
+    for i in range(n):
+        a = await wait_random(max_delay)
+        new_list.append(a)
+    for i in range(len(new_list)):
+        for j in range(len(new_list) - 1 - i):
+            if new_list[j] > new_list[j + 1]:
+                t = new_list[j]
+                new_list[j] = new_list[j + 1]
+                new_list[j + 1] = t
+    return new_list
