2f7a92794501f6e08103934d41c738668c866ce2
Akuro1411 <nahidisayev147@gmail.com>
2024-08-18 06:01:02 +0000

    The function is type annotated

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> list:
     """
     :param n: 
     :param max_delay: 
