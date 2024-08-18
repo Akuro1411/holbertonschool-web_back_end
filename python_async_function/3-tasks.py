b2116fbb8035b55df36b0581b47488b2147db1db
Akuro1411 <nahidisayev147@gmail.com>
2024-08-18 05:33:55 +0000

    Return the class

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Random and asyncio modules are imported
+"""
+import asyncio
+import random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    :param max_delay:
+    :return: The return type with asyncio.Task
+    """
+    a = asyncio.ensure_future(wait_random(max_delay))
+    return a
