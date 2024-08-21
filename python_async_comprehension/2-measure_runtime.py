f379c902ab333469f4dbb6a4d859fba3fe2e0f07
Akuro1411 <nahidisayev147@gmail.com>
2024-08-21 12:51:10 +0000

    Returns the total time when function ends

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+All the needed modules are imported
+"""
+import asyncio
+import random
+import typing
+from typing import Union
+import time
+
+
+async def measure_runtime() -> float:
+    """
+    Calculates the execution time of function and returns it
+    """
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    execution_time = time.time() - start_time
+    return execution_time
