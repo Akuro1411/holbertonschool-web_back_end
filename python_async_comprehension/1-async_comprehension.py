43838e1f6bd56a6cc4356302a8d2a28980247310
Akuro1411 <nahidisayev147@gmail.com>
2024-08-21 12:25:17 +0000

    Return can be list of integers or floats

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,7 +10,7 @@ import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> typing.List[Union[float, int]]:
+async def async_comprehension() -> typing.List[Union[float | int]]:
     """
     Funtcion returns the list of floar number which
     numbers are yielded by prev functoin
