52ae372c31ec36de5d89a16c5ec927d29a766d0a
Akuro1411 <nahidisayev147@gmail.com>
2024-08-21 12:04:50 +0000

    Returns the list of yielded numbers

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+The modules imported for async comprehension
+"""
+import asyncio
+import random
+import typing
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> [float]:
+    """
+    Funtcion returns the list of floar number which
+    numbers are yielded by prev functoin
+    """
+    arr = []
+    async for i in async_generator():
+        arr.append(i)
+    return arr
