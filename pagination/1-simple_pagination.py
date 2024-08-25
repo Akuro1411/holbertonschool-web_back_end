21309b72cd9c372f66f1b5f596c7ddc61d3926d1
Akuro1411 <nahidisayev147@gmail.com>
2024-08-25 15:38:43 +0000

    Get page code is changed

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -35,14 +35,17 @@ class Server:
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
         return self.__dataset
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page of the dataset."""
         assert isinstance(page, int) and page > 0, \
             f"page must be an integer greater than 0, got {page}"
         assert isinstance(page_size, int) and page_size > 0, \
             f"page_size must be an integer greater than 0, got {page_size}"
-        pagination_range = index_range(page, page_size)
-        data = self.dataset()
-        if pagination_range[0] >= len(data):
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
             return []
-        return data[pagination_range[0]:pagination_range[1]]
+
+        return dataset[start_index:end_index]
